7c9d3d0e9339ba418e736bed13bc4227dc69d294
jahid28 <jahidrhps123@gmail.com>
2024-05-08 12:38:46 +0530

    updates

--- server/reactServer.js
+++ server/reactServer.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
+app.use(cors());
 
 // app.use(cors({
 //   origin: ["https://twd-store.vercel.app/"],
